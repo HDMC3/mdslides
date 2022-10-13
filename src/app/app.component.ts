b1acf255c9f9cb1e93c4d41c91d5e6a99ac7ef3e
Hugo Moran <moranh56@gmail.com>
2022-10-12 23:22:26 -0600

    Add theme changing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NbThemeService } from '@nebular/theme';
 
 @Component({
     selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'mdslides';
+    currentTheme: string;
+    themeButtonIcon: string;
+
+    constructor(private nbThemeService: NbThemeService) {
+        this.currentTheme = localStorage.getItem('nbTheme') ?? 'default';
+        this.themeButtonIcon = this.currentTheme === 'default' ? 'moon-outline' : 'sun-outline';
+        this.nbThemeService.changeTheme(this.currentTheme);
+    }
+
+    changeTheme() {
+        const newTheme = this.currentTheme === 'default' ? 'dark' : 'default';
+        localStorage.setItem('nbTheme', newTheme);
+        this.currentTheme = newTheme;
+        this.nbThemeService.changeTheme(newTheme);
+        this.themeButtonIcon = newTheme === 'default' ? 'moon-outline' : 'sun-outline';
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbIconModule, NbButtonModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
@@ -17,6 +17,8 @@ import { NbEvaIconsModule } from '@nebular/eva-icons';
         BrowserAnimationsModule,
         NbThemeModule.forRoot({ name: 'default' }),
         NbLayoutModule,
+        NbIconModule,
+        NbButtonModule,
         NbEvaIconsModule
     ],
     providers: [],
