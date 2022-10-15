32759f0bf80801ee802b463055cbe0e1774cc42b
Hugo Moran <moranh56@gmail.com>
2022-10-15 12:27:45 -0600

    Add slider divider and fix editor scroll

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit, HostListener, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NbDialogService, NbSidebarService, NbThemeService } from '@nebular/theme';
 import { take } from 'rxjs/operators';
@@ -10,10 +10,16 @@ import { EditTitleDialogComponent } from '../edit-title-dialog/edit-title-dialog
     templateUrl: './editor.component.html',
     styleUrls: ['./editor.component.css']
 })
-export class EditorComponent implements OnInit {
+export class EditorComponent implements OnInit, AfterViewInit, OnDestroy {
+    @ViewChild('divider') divider?: ElementRef;
+    @ViewChild('mdEditor') mdEditor?: ElementRef;
+    @ViewChild('currentSlide') currentSlide?: ElementRef;
     currentTheme: string;
     themeButtonIcon: string;
     presentation: Presentation;
+    isClickDivider: boolean;
+    isMdEditorActive: boolean;
+    resizeObserver: ResizeObserver;
 
     constructor(
         private nbThemeService: NbThemeService,
@@ -33,12 +39,70 @@ export class EditorComponent implements OnInit {
             creation_date: new Date(),
             modification_date: new Date()
         };
+
+        this.isClickDivider = false;
+        this.isMdEditorActive = false;
+
+        this.resizeObserver = new ResizeObserver(() => {
+            if (window.innerWidth <= 500 && this.mdEditor && this.currentSlide) {
+                this.mdEditor.nativeElement.style = '';
+                this.currentSlide.nativeElement.style = '';
+            }
+        });
     }
 
     ngOnInit(): void {
         this.activatedRoute.params.pipe(take(1)).subscribe(params => {
             this.presentation.id = params['id'];
         });
+    }
+
+    ngAfterViewInit(): void {
+        this.resizeObserver.observe(document.body);
+    }
+
+    ngOnDestroy(): void {
+        this.resizeObserver.disconnect();
+    }
+
+    @HostListener('pointermove', ['$event'])
+    onMouseMoveDivider(e: PointerEvent) {
+        if (!this.isClickDivider) return;
+
+        if (!this.mdEditor || !this.currentSlide || !this.divider) return;
+
+        if (window.innerWidth > 500) {
+            const offset = e.clientX - this.divider.nativeElement.offsetLeft;
+            const newCurrentSlideWidth = this.currentSlide.nativeElement.clientWidth - offset;
+            const newMdEditorWidth = this.mdEditor.nativeElement.clientWidth + offset;
+            this.currentSlide.nativeElement.style.width = `${newCurrentSlideWidth}px`;
+            this.mdEditor.nativeElement.style.width = `${newMdEditorWidth}px`;
+        }
+    }
+
+    @HostListener('pointerdown', ['$event'])
+    onPointerDownDivider(e: any) {
+        this.isClickDivider = e.target && e.target.classList.contains('editor-section-divider');
+    }
+
+    @HostListener('pointerup', ['$event'])
+    onPointerUpDivider(e: any) {
+        this.isClickDivider = false;
+    }
+
+    switchView() {
+        if (!this.mdEditor || !this.currentSlide) return;
+
+        if (!this.isMdEditorActive) {
+            this.currentSlide.nativeElement.classList.add('hidde-panel');
+            this.mdEditor.nativeElement.classList.remove('hidde-panel');
+            this.isMdEditorActive = true;
+            return;
+        }
+
+        this.mdEditor.nativeElement.classList.add('hidde-panel');
+        this.currentSlide.nativeElement.classList.remove('hidde-panel');
+        this.isMdEditorActive = false;
     }
 
     changeTheme() {
