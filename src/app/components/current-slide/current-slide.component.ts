67948ac5b3fd984070940c8203613dae968130a6
Hugo Moran <moranh56@gmail.com>
2022-10-24 14:25:09 -0600

    Add edit slide name

--- src/app/components/current-slide/current-slide.component.ts
+++ src/app/components/current-slide/current-slide.component.ts
@@ -1,28 +1,46 @@
-import { Component, ElementRef, HostBinding } from '@angular/core';
+import { Component, ElementRef, HostBinding, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { MarpitService } from 'src/app/core/services/marpit.service';
 import { MdEditorService } from 'src/app/core/services/md-editor.service';
+import { PresentationService } from 'src/app/core/services/presentation.service';
+import { Slide } from 'src/app/data/interfaces/slide';
+import { FormValidators } from 'src/app/shared/form-validators';
 
 @Component({
     selector: 'app-current-slide',
     templateUrl: './current-slide.component.html',
     styleUrls: ['./current-slide.component.css']
 })
-export class CurrentSlideComponent {
+export class CurrentSlideComponent implements OnInit {
 
     @HostBinding('style') componentStyle = window.innerWidth <= 500 ? '--zoom: 0.3' : '--zoom: 0.4';
     editorValueStr: string;
+    editingSlideName: boolean;
+    slideNameFormControl: FormControl;
+    currentSlide?: Slide;
 
     constructor(
         private mdEditorService: MdEditorService,
         private marpitService: MarpitService,
+        private presentationService: PresentationService,
         private componentRef: ElementRef
     ) {
         this.editorValueStr = '';
+        this.editingSlideName = false;
         this.mdEditorService.editorValue.subscribe(editorChangeData => {
             const newEditorValueStr = editorChangeData.value.join('');
             if (this.editorValueStr === newEditorValueStr) return;
             this.renderSlide(editorChangeData.value);
             this.editorValueStr = newEditorValueStr;
+        });
+        this.slideNameFormControl = new FormControl('', [Validators.required, FormValidators.noEmpty]);
+    }
+
+    ngOnInit(): void {
+        this.presentationService.currentSlide$.subscribe(slide => {
+            this.editingSlideName = false;
+            this.slideNameFormControl.setValue(slide.name);
+            this.currentSlide = slide;
         });
     }
 
@@ -49,6 +67,17 @@ export class CurrentSlideComponent {
         shadowRoot.querySelectorAll('section').forEach((el: any) => {
             if (el.id !== '1') el.remove();
         });
+    }
+
+    editSlideName() {
+        this.editingSlideName = true;
+    }
+
+    saveSlideName() {
+        if (this.slideNameFormControl.invalid || !this.currentSlide) return;
+        this.currentSlide.name = this.slideNameFormControl.value;
+        this.presentationService.updateStorage();
+        this.editingSlideName = false;
     }
 
 }
