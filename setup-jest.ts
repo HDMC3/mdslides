97f8be96884ce838c6da03fb887c18f8c606ee73
Hugo Moran <moranh56@gmail.com>
2022-11-07 22:00:10 -0600

    Add crypto

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,1 +1,11 @@
 import 'jest-preset-angular/setup-jest';
+
+declare let require: any;
+// eslint-disable-next-line no-var
+var crypto = require('crypto');
+
+Object.defineProperty(globalThis.self, 'crypto', {
+    value: {
+        getRandomValues: (arr: any) => crypto.randomBytes(arr.length)
+    }
+});
